404048f67666d593bb9d58bbafe1b8f7b07a062c
tesla214 <2726573078@qq.com>
2018-05-08 00:01:19 +0800

    'conf'

---
+++ webpackMultiPagesConfig/config/auto.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  
+  entries: {
+    index: './src/assets/js/index.js',
+    about: './src/assets/js/about.js',
+    login: './src/assets/js/login.js'
+  },
+  
+  htmlWebpackPlugins: []
+  
+}
---
+++ webpackMultiPagesConfig/config/entries.js
@@ -0,0 +1,5 @@
+module.exports = {
+  index: './src/assets/js/index.js',
+  about: './src/assets/js/about.js',
+  login: './src/assets/js/login.js',
+}
